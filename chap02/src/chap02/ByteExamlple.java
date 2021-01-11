fa91864553cf1b05070713f9f5892c69a1fda3c4
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-11 11:54:40 +0900

    byte 타입 변수

---
+++ chap02/src/chap02/ByteExamlple.java
@@ -0,0 +1,18 @@
+package chap02;
+
+public class ByteExamlple {
+	public static void main(String[] args) {
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 0;
+		byte var4 = 30;
+		byte var5 = 127;
+//		byte var6 = 129; // 컴파일 에러
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+	}
+}
