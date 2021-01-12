51dda3243deb98c42007ee645d9342ddc9a6a4e4
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-12 09:20:29 +0900

    오버플로우 해결

--- chap03/src/chap03/OverflowExample.java
+++ chap03/src/chap03/OverflowExample.java
@@ -3,9 +3,9 @@ package chap03;
 public class OverflowExample {
 
 	public static void main(String[] args) {
-		int x = 1000000;
-		int y = 1000000;
-		int z = x * y;
+		long x = 1000000;
+		long y = 1000000;
+		long z = x * y;
 		System.out.println( z );
 	}
 
