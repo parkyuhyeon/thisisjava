71bd52b41871f5ff4be0c176e79c7849827d4144
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-13 10:49:29 +0900

    i를 for문 전에 선언

--- chap04/src/chap04/ForSumFrom1To100Example.java
+++ chap04/src/chap04/ForSumFrom1To100Example.java
@@ -5,11 +5,12 @@ public class ForSumFrom1To100Example {
 	public static void main(String[] args) {
 		int sum = 0;
 		
-		for (int i = 1; i <= 100; i++) {
+		int i = 0;
+		for (i = 1; i <= 100; i++) {
 			sum += i;
 		}
 		
-		System.out.println("1~100 гу : " + sum);
+		System.out.println("1~" + (i - 1) + " гу : " + sum);
 	}
 
 }
