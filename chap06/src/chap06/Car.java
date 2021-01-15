34c1dcc2ec98004e6815e513418923424b12efc1
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-15 15:50:55 +0900

    다른 생성자를 호출해서 중복 코드 줄이기

--- chap06/src/chap06/Car.java
+++ chap06/src/chap06/Car.java
@@ -6,12 +6,11 @@ public class Car {
 	}
 
 	Car(String model) {
-		this.model = model;
+		this(model, "Àº»ö", 250);
 	}
 	
 	Car(String model, String color) {
-		this.model = model;
-		this.color = color;
+		this(model, color, 250);
 	}
 	
 	Car(String model, String color, int maxSpeed) {
