735a96c2e43b78a9cfc243ed99503e426565f510
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-15 13:55:42 +0900

    생성자를 호출해서 객체 생성

--- chap06/src/chap06/CarExample.java
+++ chap06/src/chap06/CarExample.java
@@ -4,7 +4,8 @@ public class CarExample {
 
 	public static void main(String[] args) {
 		// 객체 생성
-		Car myCar = new Car();
+		Car myCar = new Car("검정", 3000);
+//		Car myCar = new Car();
 		
 		// 필드값 읽기
 		System.out.println("제작회사: " + myCar.company);
