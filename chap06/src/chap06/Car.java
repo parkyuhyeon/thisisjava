547502cc7c692abece908f0d1072455d87944778
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-15 14:57:37 +0900

    생성자의 오버로딩

--- chap06/src/chap06/Car.java
+++ chap06/src/chap06/Car.java
@@ -2,13 +2,27 @@ package chap06;
 
 public class Car {
 	// 생성자
-	Car(String color, int cc) {
+	Car() {
+	}
+
+	Car(String model) {
+		this.model = model;
+	}
+	
+	Car(String model, String color) {
+		this.model = model;
+		this.color = color;
+	}
+	
+	Car(String model, String color, int maxSpeed) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
 	}
 	
 	// 필드
 	String company = "현대자동차";
-	String model = "그랜저";
-	String color = "검정";
-	int maxSpeed = 350;
-	int speed;
+	String model;
+	String color;
+	int maxSpeed;
 }
