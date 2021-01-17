3dddff8fb984ff5eb8fa077c2588798d565e2f9f
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-17 22:08:44 +0900

    클래스 외부에서 메소드 호출

--- chap06/src/chap06/Car.java
+++ chap06/src/chap06/Car.java
@@ -4,31 +4,21 @@ public class Car {
 	// 생성자
 	
 	// 필드
-	int gas;
+	int speed;
 	
 	// 메소드
-	void setGas(int gas) {
-		this.gas = gas;
+	int getSpeed() {
+		return speed;
 	}
 	
-	boolean isLeftGas() {
-		if (gas == 0) {
-			System.out.println("gas가 없습니다.");
-			return false;	// false를 리턴
-		}
-		System.out.println("gas가 있습니다.");
-		return true;	// true를 리턴
+	void keyTurnOn() {
+		System.out.println("키를 돌립니다.");
 	}
 	
 	void run() {
-		while (true) {
-			if (gas > 0) {
-				System.out.println("달립니다.(gas잔량:" + gas + ")");
-				gas -= 1;
-			} else {
-				System.out.println("멈춥니다.(gas잔량:" + gas + ")");
-				return;	// 메소드 실행 종료
-			}
+		for (int i = 0; i <= 50; i += 10) {
+			speed = i;
+			System.out.println("달립니다.(시속:" + speed + "km/h)");
 		}
 	}
 }
--- chap06/src/chap06/CarExample.java
+++ chap06/src/chap06/CarExample.java
@@ -4,21 +4,10 @@ public class CarExample {
 
 	public static void main(String[] args) {
 		Car myCar = new Car();
-		
-		myCar.setGas(5);	// Car의 setGas() 메소드 호출
-		
-		boolean gasState = myCar.isLeftGas();	// Car의 isLeftGas() 메소드 호출
-		
-		if (gasState) {
-			System.out.println("출발합니다.");
-			myCar.run();	// Car의 run() 메소드 호출
-		}
-		
-		if (myCar.isLeftGas()) {	// Car의 isLeftGas() 메소드 호출
-			System.out.println("gas를 주입할 필요가 없습니다.");
-		} else {
-			System.out.println("gas를 주입하세요.");
-		}
+		myCar.keyTurnOn();
+		myCar.run();
+		int speed = myCar.getSpeed();
+		System.out.println("현재 속도: " + speed + "km/h");
 	}
 
 }
