aac722cf1b1fadf7c821eb39c0e0ac12c3878ba3
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-17 22:55:02 +0900

    인스턴스 멤버와 this

---
+++ chap06/src/sec09/CarExample.java
@@ -0,0 +1,13 @@
+package sec09;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car("Æ÷¸£½¦");
+		Car yourCar = new Car("º¥Ã÷");
+		
+		myCar.run();
+		yourCar.run();
+	}
+
+}
