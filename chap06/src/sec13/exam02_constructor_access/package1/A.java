9690158d934bae2c687f25b0ba7736a0959d03d9
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-18 10:04:48 +0900

    생성자의 접근 제한

---
+++ chap06/src/sec13/exam02_constructor_access/package1/A.java
@@ -0,0 +1,13 @@
+package sec13.exam02_constructor_access.package1;
+
+public class A {
+	// 필드
+	A a1 = new A(true);		// (o)
+	A a2 = new A(1);		// (o)
+	A a3 = new A("문자열");	// (o)
+	
+	// 생성자
+	public A(boolean b) {}
+	A(int b) {}
+	private A(String s) {}
+}
