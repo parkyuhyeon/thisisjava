fbb813289a5e1da1f9e6f3740a03cfc58e505b27
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-18 11:10:22 +0900

    어노테이션 정의

---
+++ chap06/src/sec15/PrintAnnotation.java
@@ -0,0 +1,10 @@
+package sec15;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PrintAnnotation {
+	String value() default "-";
+	int number() default 15;
+}
