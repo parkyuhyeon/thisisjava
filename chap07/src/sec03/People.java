14c271939102cd3f8aa6fa3547637990066179e2
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-19 04:58:18 +0900

    오타 ssn 수정

--- chap07/src/sec03/People.java
+++ chap07/src/sec03/People.java
@@ -4,7 +4,7 @@ public class People {
 	public String name;
 	public String ssn;
 	
-	public People(String name, String ssm) {
+	public People(String name, String ssn) {
 		this.name = name;
 		this.ssn = ssn;
 	}
