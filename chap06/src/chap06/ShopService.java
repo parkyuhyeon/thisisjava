7e22876dd709eab844de10efda5103b99980dae5
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-18 15:10:38 +0900

    확인문제

---
+++ chap06/src/chap06/ShopService.java
@@ -0,0 +1,11 @@
+package chap06;
+
+public class ShopService {
+	private static ShopService singleton = new ShopService();
+	
+	private ShopService() {}
+	
+	static ShopService getInstance() {
+		return singleton;
+	}
+}
