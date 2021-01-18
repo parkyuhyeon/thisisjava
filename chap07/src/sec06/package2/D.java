92a1a91e3ac21842c0bd8bd88b185286ade34ef4
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-19 07:34:31 +0900

    protected 접근 제한자

---
+++ chap07/src/sec06/package2/D.java
@@ -0,0 +1,11 @@
+package sec06.package2;
+
+import sec06.package1.A;
+
+public class D extends A {
+	public D() {
+		super();				// (o)
+		this.field = "value";	// (o)
+		this.method();			// (o)
+	}
+}
