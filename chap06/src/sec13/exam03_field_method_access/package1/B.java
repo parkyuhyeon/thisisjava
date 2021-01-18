723b9725d102a271b6f890eaa75a3df6e706d807
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-18 10:16:52 +0900

    필드와 메소드의 접근 제한

---
+++ chap06/src/sec13/exam03_field_method_access/package1/B.java
@@ -0,0 +1,14 @@
+package sec13.exam03_field_method_access.package1;
+
+public class B {
+	public B() {
+		A a = new A();
+		a.field1 = 1;	// (o)
+		a.field2 = 1;	// (o)
+		a.field3 = 1;	// (x)
+		
+		a.method1();	// (o)
+		a.method2();	// (o)
+		a.method3();	// (x)
+	}
+}
