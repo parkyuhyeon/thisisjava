a0caa597ad9e0d75e82a3a7a3481cf3628387aad
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-19 13:35:32 +0900

    타이어 필드를 배열로 수정

--- chap07/src/sec07/Car.java
+++ chap07/src/sec07/Car.java
@@ -2,10 +2,16 @@ package sec07;
 
 public class Car {
 	// 필드
-	Tire frontLeftTire = new Tire("앞왼쪽", 6);
-	Tire frontRightTire = new Tire("앞오른쪽", 2);
-	Tire backLeftTire = new Tire("뒤왼쪽", 3);
-	Tire backRightTire = new Tire("뒤오른쪽", 4);
+//	Tire frontLeftTire = new Tire("앞왼쪽", 6);
+//	Tire frontRightTire = new Tire("앞오른쪽", 2);
+//	Tire backLeftTire = new Tire("뒤왼쪽", 3);
+//	Tire backRightTire = new Tire("뒤오른쪽", 4);
+	Tire[] tires = {
+			new Tire("앞왼쪽", 6),
+			new Tire("앞오른쪽", 2),
+			new Tire("뒤왼쪽", 3),
+			new Tire("뒤오른쪽", 4)
+	};
 	
 	// 생성자
 	
@@ -13,13 +19,19 @@ public class Car {
 	int run() {
 		System.out.println("[자동차가 달립니다.]");
 		
-		if (frontLeftTire.roll() == false) { stop(); return 1; }
-		
-		if (frontRightTire.roll() == false) { stop(); return 2; }
-		
-		if (backLeftTire.roll() == false) { stop(); return 3; }
-		
-		if (backRightTire.roll() == false) { stop(); return 4; }
+//		if (frontLeftTire.roll() == false) { stop(); return 1; }
+//		
+//		if (frontRightTire.roll() == false) { stop(); return 2; }
+//		
+//		if (backLeftTire.roll() == false) { stop(); return 3; }
+//		
+//		if (backRightTire.roll() == false) { stop(); return 4; }
+		for (int i = 0; i < tires.length; i++) {
+			if (tires[i].roll() == false) {
+				stop();
+				return (i + 1);
+			}
+		}
 		
 		return 0;
 	}
