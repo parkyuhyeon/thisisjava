5085db9749ef4c0744ff84c8d7826014d37ad240
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-19 12:55:44 +0900

    Tire의 자식 클래스

---
+++ chap07/src/sec07/KumhoTire.java
@@ -0,0 +1,24 @@
+package sec07;
+
+public class KumhoTire extends Tire {
+	// 필드
+	
+	// 생성자
+	public KumhoTire(String location, int maxRotation) {
+		super(location, maxRotation);
+	}
+	
+	//메소드
+	@Override
+	public boolean roll() {
+		++accumlatedRotation;
+		if (accumlatedRotation < maxRotation) {
+			System.out.println(location + " KumhoTire 수명: " +
+					(maxRotation - accumlatedRotation) + "회");
+			return true;
+		} else {
+			System.out.println("*** " + location + " HankooKumhoTire 펑크 ***");
+			return false;
+		}
+	}
+}
