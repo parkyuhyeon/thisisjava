a66ec272f666e3d3505678b42b29ecb301bdbe8f
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-19 14:56:11 +0900

    추상 클래스

---
+++ chap07/src/sec08/exam03/PhoneExample.java
@@ -0,0 +1,14 @@
+package sec08.exam03;
+
+public class PhoneExample {
+
+	public static void main(String[] args) {
+//		Phone phone = new Phone();
+		SmartPhone smartPhone = new SmartPhone("ȫ�浿");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+	}
+
+}
