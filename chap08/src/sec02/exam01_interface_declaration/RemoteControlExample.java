d8c6053f6fa9277b4ae7021cf711033648a96241
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-20 11:40:45 +0900

    인터페이스 변수에 구현 객체 대입

---
+++ chap08/src/sec02/exam01_interface_declaration/RemoteControlExample.java
@@ -0,0 +1,11 @@
+package sec02.exam01_interface_declaration;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+	}
+
+}
