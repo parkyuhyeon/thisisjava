3cfb19801a802da0a65d519d34424dc0a163be07
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-20 12:02:41 +0900

    정적 메소드 사용

---
+++ chap08/src/sec04/exam03/RemoteControlExample.java
@@ -0,0 +1,11 @@
+package sec04.exam03;
+
+import sec02.exam01_interface_declaration.*;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		RemoteControl.changeBattery();
+	}
+
+}
