51bb5ee85eae3aacff6c43a2de8d0958868b5625
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-20 13:09:42 +0900

    필드 다형성 테스트

---
+++ chap08/src/sec05/exam02/CarExample.java
@@ -0,0 +1,16 @@
+package sec05.exam02;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car();
+		
+		myCar.run();
+		
+		myCar.frontLeftTire = new KumhoTire();
+		myCar.frontRightTire = new KumhoTire();
+		
+		myCar.run();
+	}
+
+}
