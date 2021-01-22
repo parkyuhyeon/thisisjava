d6233f80a34ecef6d3a9b107137e5216fc75d327
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-22 16:36:54 +0900

    실행 예외 처리

--- chap10/src/sec03/TryCatchFinallyExample.java
+++ chap10/src/sec03/TryCatchFinallyExample.java
@@ -4,7 +4,7 @@ public class TryCatchFinallyExample {
 
 	public static void main(String[] args) {
 		try {
-			Class clazz = Class.forName("java.lang.String2");
+			Class clazz = Class.forName("java.lang.String");
 		} catch (ClassNotFoundException e) {
 			System.out.println("클래스가 존재하지 않습니다.");
 		}
