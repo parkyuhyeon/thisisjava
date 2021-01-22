e103a686ddb0a812f6bf0c4e75b84dd3de498063
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-22 13:08:48 +0900

    ArrayIndexOutOfBoundsException

--- chap10/src/sec02/exam02/ArrayIndexOutOfBoundsExceptionExample.java
+++ chap10/src/sec02/exam02/ArrayIndexOutOfBoundsExceptionExample.java
@@ -3,11 +3,17 @@ package sec02.exam02;
 public class ArrayIndexOutOfBoundsExceptionExample {
 
 	public static void main(String[] args) {
-		String data1 = args[0];
-		String data2 = args[1];
-		
-		System.out.println("args[0]: " + data1);
-		System.out.println("args[1]: " + data2);
+		if (args.length == 2) {
+			String data1 = args[0];
+			String data2 = args[1];
+			
+			System.out.println("args[0]: " + data1);
+			System.out.println("args[1]: " + data2);
+		} else {
+			System.out.println("[실행 방법]");
+			System.out.print("java ArrayIndexOutOfBoundsExceptionExample ");
+			System.out.print("값1 값2");
+		}
 	}
 
 }
