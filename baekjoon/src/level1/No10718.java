c2ec4883717137ebebfcd836f2f79f4fea5382d6
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-25 11:34:43 +0900

    Level 1

---
+++ baekjoon/src/level1/No10718.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class No10718 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 2; i++) {
+            System.out.println("강한친구 대한육군");
+        }
+	}
+
+}
---
+++ baekjoon/src/level1/No2557.java
@@ -0,0 +1,7 @@
+package level1;
+
+public class No2557 {
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
