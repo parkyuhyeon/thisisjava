d3710a7d1ac06607e2cc5bff78123791d7ec2b7f
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-02 10:33:17 +0900

    동적 객체 생성 및 실행

---
+++ chap11/src/sec06/exam03/NewInstanceExample.java
@@ -0,0 +1,20 @@
+package sec06.exam03;
+
+public class NewInstanceExample {
+
+	public static void main(String[] args) {
+		try {
+//			Class clazz = Class.forName("sec06.exam03.SendAction");
+			Class clazz = Class.forName("sec06.exam03.ReceiveAction");
+			Action action = (Action) clazz.newInstance();
+			action.execute();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
