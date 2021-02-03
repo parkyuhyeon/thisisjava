ce0dd7d1844242a984d4234ce0141b933cf1b197
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-03 15:52:25 +0900

    Comparable 구현 클래스

---
+++ chap11/src/sec11/exam03/Member.java
@@ -0,0 +1,16 @@
+package sec11.exam03;
+
+public class Member implements Comparable<Member> {
+
+	String name;
+	
+	Member(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public int compareTo(Member o) {
+		return name.compareTo(o.name);
+	}
+
+}
