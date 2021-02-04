a7b904c2f579e8436fc224230452dc51bb00b7be
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-04 18:55:52 +0900

    임의의 주사위의 눈 얻기

---
+++ chap11/src/sec13/exam01/MathRandomExample.java
@@ -0,0 +1,10 @@
+package sec13.exam01;
+
+public class MathRandomExample {
+
+	public static void main(String[] args) {
+		int num = (int) (Math.random() * 6) + 1;
+		System.out.println("주사위의 눈: " + num);
+	}
+
+}
