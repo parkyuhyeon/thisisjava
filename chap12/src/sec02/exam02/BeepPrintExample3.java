fc7eb5f72e578b3ab4b2ec2c2c4e49c490595dd3
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-08 16:57:21 +0900

    메인 스레드와 작업 스레드가 동시에 실행

--- chap12/src/sec02/exam02/BeepPrintExample3.java
+++ chap12/src/sec02/exam02/BeepPrintExample3.java
@@ -1,9 +1,23 @@
 package sec02.exam02;
 
+import java.awt.Toolkit;
+
 public class BeepPrintExample3 {
 
 	public static void main(String[] args) {
-		Thread thread = new BeepThread();
+//		Thread thread = new BeepThread();
+		Thread thread = new Thread() {
+			@Override
+			public void run() {
+				Toolkit toolkit = Toolkit.getDefaultToolkit();
+				for (int i = 0; i <5; i++) {
+					toolkit.beep();
+					try {
+						Thread.sleep(500);
+					} catch (Exception e) {	}
+				}
+			}
+		};
 		thread.start();
 		
 		for (int i = 0; i < 5; i++) {
