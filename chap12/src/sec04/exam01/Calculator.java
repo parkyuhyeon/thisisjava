383de340af2d35eac4c29e447a15dbb6a0bccd48
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-08 19:21:05 +0900

    동기화 메소드로 수정된 공유 객체

--- chap12/src/sec04/exam01/Calculator.java
+++ chap12/src/sec04/exam01/Calculator.java
@@ -7,11 +7,20 @@ public class Calculator {
 		return memory;
 	}
 	
-	public synchronized void setMemory(int memory) {
-		this.memory = memory;
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {}
-		System.out.println(Thread.currentThread().getName() + ": " + this.memory);
+//	public synchronized void setMemory(int memory) {
+//		this.memory = memory;
+//		try {
+//			Thread.sleep(2000);
+//		} catch (InterruptedException e) {}
+//		System.out.println(Thread.currentThread().getName() + ": " + this.memory);
+//	}
+	public void setMemory(int memory) {
+		synchronized (this) {
+			this.memory = memory;
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {}
+			System.out.println(Thread.currentThread().getName() + ": " + this.memory);
+		}
 	}
 }
