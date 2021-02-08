28e23e71c66e6de58a4b6114c3b2fabe5c208b7f
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-08 20:37:40 +0900

    다른 스레드가 종료될 때까지 일시 정지 상태 유지

---
+++ chap12/src/sec06/exam03/JoinExample.java
@@ -0,0 +1,16 @@
+package sec06.exam03;
+
+public class JoinExample {
+
+	public static void main(String[] args) {
+		SumThread sumThread = new SumThread();
+		sumThread.start();
+		
+		try {
+			sumThread.join();
+		} catch (InterruptedException e) {}
+		
+		System.out.println("1~100 гу: " + sumThread.getSum());
+	}
+
+}
