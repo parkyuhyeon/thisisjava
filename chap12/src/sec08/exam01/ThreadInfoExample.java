35dd5b56d132d9f643195b89a5ed3c83e13dc2ff
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-09 11:44:02 +0900

    현재 실행 중인 스레드 정보

---
+++ chap12/src/sec08/exam01/ThreadInfoExample.java
@@ -0,0 +1,27 @@
+package sec08.exam01;
+
+import java.util.Map;
+import java.util.Set;
+
+import sec07.AutoSaveThread;
+
+public class ThreadInfoExample {
+
+	public static void main(String[] args) {
+		AutoSaveThread autoSaveThread = new AutoSaveThread();
+		autoSaveThread.setName("AutoSaveThread");
+		autoSaveThread.setDaemon(true);
+		autoSaveThread.start();
+		
+		Map<Thread, StackTraceElement[]> map = Thread.getAllStackTraces();
+		Set<Thread> threads = map.keySet();
+		
+		for (Thread thread : threads) {
+			System.out.println("Name: " + thread.getName() + 
+				(thread.isDaemon() ? "(单阁)" : "(林)"));
+			System.out.println("\t" + "家加弊缝: " + thread.getThreadGroup().getName());
+			System.out.println();
+		}
+	}
+
+}
