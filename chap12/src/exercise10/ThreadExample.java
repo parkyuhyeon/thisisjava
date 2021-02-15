976aa7080fcfadf3f9e4248a3173d90678d65136
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-15 20:13:28 +0900

    확인문제

---
+++ chap12/src/exercise10/ThreadExample.java
@@ -0,0 +1,13 @@
+package exercise10;
+
+public class ThreadExample {
+
+	public static void main(String[] args) {
+		Thread thread = new MovieThread();
+		thread.setDaemon(true);
+		thread.start();
+		
+		try { Thread.sleep(1000); } catch (InterruptedException e) { }
+	}
+
+}
