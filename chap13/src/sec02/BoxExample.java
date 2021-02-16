91e4a732fe34d657421c38f1c9c29eb23d074239
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-16 20:37:59 +0900

    제네릭 타입 이용

--- chap13/src/sec02/BoxExample.java
+++ chap13/src/sec02/BoxExample.java
@@ -3,12 +3,13 @@ package sec02;
 public class BoxExample {
 
 	public static void main(String[] args) {
-		Box box = new Box();
-		box.set("홍길동");					// String -> Object (자동 타입 변환)
-		String name = (String) box.get();	// Object -> String (강제 타입 변환)
+		Box<String> box1 = new Box<String>();
+		box1.set("hello");
+		String str = box1.get();
 		
-		box.set(new Apple());				// Apple -> Object (자동 타입 변환)
-		Apple apple = (Apple) box.get();	// Object -> Apple (강제 타입 변환)
+		Box<Integer> box2 = new Box<Integer>();
+		box2.set(6);
+		int value = box2.get();
 	}
 
 }
