a65ebc327685d941a1ca3536cffb436f076f20dd
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-16 20:34:39 +0900

    제네릭 타입

--- chap13/src/sec02/Box.java
+++ chap13/src/sec02/Box.java
@@ -1,7 +1,7 @@
 package sec02;
 
-public class Box {
-	private Object object;
-	public void set(Object object) { this.object = object; }
-	public Object get() { return object; }
+public class Box<T> {
+	private T t;
+	public T get() { return t; }
+	public void set(T t) { this.t = t; }
 }
