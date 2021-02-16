8aa0d3b979824f3d90c59876c46fc9b9d6f18121
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-16 18:42:20 +0900

    비제네릭 타입 이용

---
+++ chap13/src/sec02/BoxExample.java
@@ -0,0 +1,14 @@
+package sec02;
+
+public class BoxExample {
+
+	public static void main(String[] args) {
+		Box box = new Box();
+		box.set("홍길동");					// String -> Object (자동 타입 변환)
+		String name = (String) box.get();	// Object -> String (강제 타입 변환)
+		
+		box.set(new Apple());				// Apple -> Object (자동 타입 변환)
+		Apple apple = (Apple) box.get();	// Object -> Apple (강제 타입 변환)
+	}
+
+}
