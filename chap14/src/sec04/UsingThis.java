b419f32681dde9658f173acbb2182bf38613e5e5
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-03-08 17:13:01 +0900

    this 사용

---
+++ chap14/src/sec04/UsingThis.java
@@ -0,0 +1,21 @@
+package sec04;
+
+public class UsingThis {
+	public int outterField = 10;
+	
+	class Innter {
+		int innerFiled = 20;
+		
+		void method() {
+			// ¶÷´Ù½Ä
+			MyFunctionalInterface fi = () -> {
+				System.out.println("outterField: " + outterField);
+				System.out.println("outterField: " + UsingThis.this.outterField + "\n");
+				
+				System.out.println("innerField: " + innerFiled);
+				System.out.println("innerField: " + this.innerFiled + "\n");
+			};
+			fi.method();
+		}
+	}
+}
