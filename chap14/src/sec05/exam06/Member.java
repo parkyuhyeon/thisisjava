c2e472c88575269782483a2785199b867ad97d9e
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-03-21 17:39:28 +0900

    회원 클래스

---
+++ chap14/src/sec05/exam06/Member.java
@@ -0,0 +1,17 @@
+package sec05.exam06;
+
+public class Member {
+	private String name;
+	private String id;
+	private String address;
+	
+	public Member(String name, String id, String address) {
+		this.name = name;
+		this.id = id;
+		this.address = address;
+	}
+	
+	public String getName() { return name; }
+	public String getId() { return id; }
+	public String getAddress() { return address; }
+}
