9eff2edb532eb0942b44d7bc33c2a29b14334a9d
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-03-21 17:43:22 +0900

    Consumer의 순차적 연결

---
+++ chap14/src/sec05/exam06/ConsumerAndThenExample.java
@@ -0,0 +1,20 @@
+package sec05.exam06;
+
+import java.util.function.Consumer;
+
+public class ConsumerAndThenExample {
+
+	public static void main(String[] args) {
+		Consumer<Member> consumerA = (m) -> {
+			System.out.println("consumerA: " + m.getName());
+		};
+		
+		Consumer<Member> consumerB = m -> {
+			System.out.println("consumerB: " + m.getId());
+		};
+		
+		Consumer<Member> consumerAB = consumerA.andThen(consumerB);
+		consumerAB.accept(new Member("ȫ�浿", "hong", null));
+	}
+
+}
