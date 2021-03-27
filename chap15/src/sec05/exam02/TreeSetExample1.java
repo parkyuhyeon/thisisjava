8ef7e78c3781230d803989cad780da3e62cebce1
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-03-27 13:27:33 +0900

    특정 객체 찾기

--- chap15/src/sec05/exam02/TreeSetExample1.java
+++ chap15/src/sec05/exam02/TreeSetExample1.java
@@ -2,7 +2,7 @@ package sec05.exam02;
 
 import java.util.TreeSet;
 
-public class TreeSetExample {
+public class TreeSetExample1 {
 
 	public static void main(String[] args) {
 		TreeSet<Integer> scores = new TreeSet<Integer>();
