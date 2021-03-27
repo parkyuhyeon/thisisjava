3e3431a25dd9331d399f2023d13ac9289f22f47e
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-03-27 11:05:11 +0900

    Arrays.asList() 메소드

---
+++ chap15/src/sec02/exam01/ArraysAsListExample.java
@@ -0,0 +1,20 @@
+package sec02.exam01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysAsListExample {
+
+	public static void main(String[] args) {
+		List<String> list1 = Arrays.asList("È«±æµ¿", "½Å¿ë±Ç", "±èÀÚ¹Ù");
+		for (String name : list1) {
+			System.out.println(name);
+		}
+		
+		List<Integer> list2 = Arrays.asList(1, 2, 3);
+		for(int value : list2) {
+			System.out.println(value);
+		}
+	}
+
+}
