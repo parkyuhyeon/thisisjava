67782bbe6ebee5f1acff2c77ec4fc48829a427d7
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-03-27 18:11:33 +0900

    컬렉션으로부터 스트림 얻기

---
+++ chap16/src/sec02/exam01/FromCollectionExample.java
@@ -0,0 +1,22 @@
+package sec02.exam01;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import sec01.exam02.Student;
+
+public class FromCollectionExample {
+
+	public static void main(String[] args) {
+		List<Student> studentList = Arrays.asList(
+			new Student("홍길동", 10),
+			new Student("신용권", 20),
+			new Student("유미선", 30)
+		);
+		
+		Stream<Student> stream = studentList.stream();
+		stream.forEach(s -> System.out.println(s.getName()));
+	}
+
+}
