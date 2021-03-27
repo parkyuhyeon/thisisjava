add9c15f0c20e1373728a7666b3472d5a6fb0c1a
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-03-27 18:17:00 +0900

    정수 범위를 소스로 하는 스트림

---
+++ chap16/src/sec02/exam03/FromIntRangeExample.java
@@ -0,0 +1,14 @@
+package sec02.exam03;
+
+import java.util.stream.IntStream;
+
+public class FromIntRangeExample {
+	public static int sum;
+	
+	public static void main(String[] args) {
+		IntStream stream = IntStream.rangeClosed(1, 100);
+		stream.forEach(a -> sum += a);
+		System.out.println("รัวี: " + sum);
+	}
+
+}
